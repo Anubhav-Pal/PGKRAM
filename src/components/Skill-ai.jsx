1deee5c13cb20cb384209a858e0b2576b909c5ac
Vanshika <vanshikajain8750@gmail.com>
2023-12-19 01:08:59 +0530

    enhanced

---
+++ src/components/Skill-ai.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skillai = (props) => {
+  return (
+    <div className='border-2 border-black py-1 px-2 inline rounded-lg mx-4'>
+        {props.skill}
+    </div>
+  )
+}
+
+export default Skillai
--- src/pages/userSide/AIInterview.jsx
+++ src/pages/userSide/AIInterview.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Skillai from '../../components/Skill-ai'
 
 const AIInterview = () => {
   return (
-    <div>AIInterview</div>
+    <div>
+    <div className=' m-10 h-[50vh] shadow-2xl p-10'>
+      <div className='text-gray-600 my-8'>Choose your skills, and let our AI generate tailored questions</div>
+
+      <div>
+        <Skillai skill='python'></Skillai>
+        <Skillai skill='python'></Skillai><Skillai skill='python'></Skillai><Skillai skill='python'></Skillai><Skillai skill='python'></Skillai><Skillai skill='python'></Skillai><Skillai skill='python'></Skillai><Skillai skill='python'></Skillai>
+      </div>
+    </div>
+    </div>
   )
 }
 
