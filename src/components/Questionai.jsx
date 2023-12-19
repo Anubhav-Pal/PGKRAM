2379133142b0bcc64981bf33023a2d0dd694d072
Vanshika <vanshikajain8750@gmail.com>
2023-12-19 10:03:24 +0530

    skills select added

---
+++ src/components/Questionai.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Questionui from './Questionui'
+
+const Questionai = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                Skip Question
+            </div>
+        <Questionui/>
+        <div>NEXT</div>
+        </div>
+    </div>
+  )
+}
+
+export default Questionai
