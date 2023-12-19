105e4557b610da3f5b8311577525a780006c9124
Anubhav Pal <anubhavpal15june@gmail.com>
2023-12-19 13:17:24 +0530

    commit

--- src/pages/userSide/Jobs.jsx
+++ src/pages/userSide/Jobs.jsx
@@ -1,55 +1,18 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import searchicon from '../../assets/searchicon.svg';
 import JobDisplay from '../../components/JobDisplay';
 import Featured from '../../components/Featured';
 import axios from 'axios';
 const Jobs = () => {
-  const [data,setData]=useState(null);
-  
-  const [jobs, setJobs] = useState([
-    {
-      id: 1,
-      icon: 'icon1',
-      position: 'Frontend Developer',
-      company: 'Tech Co.',
-      medium: 'Remote only',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
-    {
-      id: 2,
-      icon: 'icon2',
-      position: 'UX/UI Designer',
-      company: 'Design Studio',
-      medium: 'On-site',
-      text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-    },
-    {
-      id: 3,
-      icon: 'icon3',
-      position: 'backend Developer',
-      company: 'innovation Co.',
-      medium: 'onsite only',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
-    {
-      id: 4,
-      icon: 'icon4',
-      position: 'SDE',
-      company: 'jss',
-      medium: 'onsite only',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
-    // Add more dummy jobs as needed
-  ]);
-
+  const [data, setData] = useState({});
   const [hasPremium, setHasPremium] = useState(false);
   const [showPremiumPopup, setShowPremiumPopup] = useState(false);
+
+
   const handleJobFinderClick = () => {
     if (!hasPremium) {
-      // User doesn't have premium, show popup
       setShowPremiumPopup(true);
     } else {
-      // Handle premium user logic
     }
   };
 
@@ -59,7 +22,7 @@ const Jobs = () => {
 
   const handleSaveJob = (id) => {
     const savedJobs = JSON.parse(localStorage.getItem('savedJobs')) || {};
-  
+
     // Check if the job ID is already saved
     if (savedJobs[id]) {
       console.log('Job is already saved');
@@ -69,25 +32,30 @@ const Jobs = () => {
       console.log('Job saved successfully');
     }
   };
-  
-  
 
   const handlePopupClose = () => {
     setShowPremiumPopup(false);
   };
+
+  // fetching the data
   useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get('https://pgrkam-backend.onrender.com/get-jobs');
-          setData(response.data);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-  
-      fetchData();
-    },[])
-  console.log(data)
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://pgrkam-backend.onrender.com/get-jobs');
+        setData(response.data['Featured']);
+        // console.log(data);
+
+
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [])
+
+
   return (
     <div className='flex m-auto py-4 bg-gray-100'>
       {/* 1st col */}
@@ -95,108 +63,99 @@ const Jobs = () => {
 
         {/* search */}
         <div className='flex m-auto'>
-        <div className='bg-white justify-between items-center px-10 py-1 rounded-md flex w-4/5 m-auto'>
-          <div className='w-full'><input className='w-full py-1 px-2' type='text' placeholder='Search'/></div>
-          <button><img  className='bg-[#ED9017] rounded-md p-2' src={searchicon} alt="searchicon" /></button>
-        </div>
-        <div className='1/5 m-auto'>
-          <button onClick={handleJobFinderClick} className='py-2 px-6 rounded-lg text-white font-semibold bg-[#ED9017]'>Job Finder</button>
-        </div>
-        </div>
-
- {/* Premium Popup */}
- {showPremiumPopup && (
-        <div className='fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-white bg-opacity-70'
-        >
-          <div className='my-8 text-[24px]  text-center font-semibold'>Join <span className='text-[#4f2ce0] font-bold'>PGRKAM Premium </span> and get access to career - boosting features. </div>
-          <div className='bg-white w-[40vw] p-8 rounded-md shadow-2xl'>
-            {/* Close button */}
-           <div className='flex justify-end'> <button
-              className='flex p-2 cursor-pointer'
-              onClick={handlePopupClose}
-            >
-              &times;
-            </button></div>
-
-            {/* Popup content */}
-            <div className='my-4 '>
-            <h2 className='text-xl font-semibold mb-4 text-center'>Premium Information</h2>
-            <ul className='list-disc my-auto'>
-              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ab!</li>
-              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos provident exercitationem accusantium recusandae modi quidem vitae consequatur perferendis sit repudiandae sint repellendus voluptatum beatae velit natus, neque fugit tempora corporis.  </li>
-              <li>Lorem ipsum dolor sit amet.</li>
-              </ul>
-
-              <div className='text-center my-4'>
-              <button className='rounded-lg py-2 px-8 bg-[#ED9017] text-white font-bold text-center'>
-                XXX / Month
-              </button>
-              </div>
-            </div>
-            {/* Add more premium details as needed */}
+          <div className='bg-white justify-between items-center px-10 py-1 rounded-md flex w-4/5 m-auto'>
+            <div className='w-full'><input className='w-full py-1 px-2' type='text' placeholder='Search' /></div>
+            <button><img className='bg-[#ED9017] rounded-md p-2' src={searchicon} alt="searchicon" /></button>
+          </div>
+          <div className='1/5 m-auto'>
+            <button onClick={handleJobFinderClick} className='py-2 px-6 rounded-lg text-white font-semibold bg-[#ED9017]'>Job Finder</button>
           </div>
         </div>
-      )}
+
+        {/* Premium Popup */}
+        {showPremiumPopup && (
+          <div className='fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-white bg-opacity-70'
+          >
+            <div className='my-8 text-[24px]  text-center font-semibold'>Join <span className='text-[#4f2ce0] font-bold'>PGRKAM Premium </span> and get access to career - boosting features. </div>
+            <div className='bg-white w-[40vw] p-8 rounded-md shadow-2xl'>
+              {/* Close button */}
+              <div className='flex justify-end'> <button
+                className='flex p-2 cursor-pointer'
+                onClick={handlePopupClose}
+              >
+                &times;
+              </button></div>
+
+              {/* Popup content */}
+              <div className='my-4 '>
+                <h2 className='text-xl font-semibold mb-4 text-center'>Premium Information</h2>
+                <ul className='list-disc my-auto'>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ab!</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos provident exercitationem accusantium recusandae modi quidem vitae consequatur perferendis sit repudiandae sint repellendus voluptatum beatae velit natus, neque fugit tempora corporis.  </li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                </ul>
+
+                <div className='text-center my-4'>
+                  <button className='rounded-lg py-2 px-8 bg-[#ED9017] text-white font-bold text-center'>
+                    XXX / Month
+                  </button>
+                </div>
+              </div>
+              {/* Add more premium details as needed */}
+            </div>
+          </div>
+        )}
 
         {/* filters */}
-    <div className='flex m-auto'>
-      
-   <div className='mx-auto my-4 '>
-   <select className='py-1 px-2 '>
-        <option value="Salary"> Salary</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </select>
-   </div>
+        <div className='flex m-auto'>
 
-   <div className='mx-auto my-4 '>
-   <select className='py-1 px-2 '>
-        <option value="Salary"> Type of Workplace</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </select>
-   </div>
+          <div className='mx-auto my-4 '>
+            <select className='py-1 px-2 '>
+              <option value="Salary"> Salary</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+          </div>
 
-   <div className='mx-auto my-4 '>
-   <select className='py-1 px-2 '>
-        <option value="Salary"> Experience Level</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </select>
-   </div>
+          <div className='mx-auto my-4 '>
+            <select className='py-1 px-2 '>
+              <option value="Salary"> Type of Workplace</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+          </div>
 
-   <div className='mx-auto my-4 '>
-   <select className='py-1 px-2 '>
-        <option value="Salary">Distance</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </select>
-   </div>
+          <div className='mx-auto my-4 '>
+            <select className='py-1 px-2 '>
+              <option value="Salary"> Experience Level</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+          </div>
 
-    </div>
+          <div className='mx-auto my-4 '>
+            <select className='py-1 px-2 '>
+              <option value="Salary">Distance</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+          </div>
 
-    {/* start of jobs */}
-    <div className='text-[14px] text-center my-4'>RECOMMENDED JOBS FOR YOU</div>
+        </div>
 
-    {/* jobs */}
-    {jobs.map(job => (
-  <JobDisplay
-    key={job.id}
-    id={job.id} 
-    icon={job.icon}
-    position={job.position}
-    company={job.company}
-    medium={job.medium}
-    text={job.text}
-    onRemove={() => handleRemoveJob(job.id)}
-    onSave={() => handleSaveJob(job.id)}
-  />
-))}
+        {/* start of jobs */}
+        <div className='text-[14px] text-center my-4'>RECOMMENDED JOBS FOR YOU</div>
+
+        {/* jobs */}
+        {Object.values(data).map(jobData => (
+          <JobDisplay jobData={jobData} />
+        ))}
+
       </div>
 
-{/* 2nd col */}
+      {/* 2nd col */}
       <div className='w-1/5 m-auto'>
-        <Featured/>
+        <Featured  />
       </div>
     </div>
   )
