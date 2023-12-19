685b65d78e9a940f204ad62db0e3bcb5b317d0c9
Vanshika <vanshikajain8750@gmail.com>
2023-12-19 11:00:17 +0530

    ai iinterview

--- src/components/Questionui.jsx
+++ src/components/Questionui.jsx
@@ -1,70 +1,63 @@
+// Questionui.jsx
+
 import React, { useState, useRef } from 'react';
 import RecordRTC from 'recordrtc';
 
-const Questionui = () => {
+const Questionui = ({ question }) => {
   const videoRef = useRef(null);
+  const previewVideoRef = useRef(null);
   const [recording, setRecording] = useState(false);
   const [recordedVideoBlob, setRecordedVideoBlob] = useState(null);
-
-  // Declare recorder as a state variable
   const [recorder, setRecorder] = useState(null);
 
-  const startRecording = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-    videoRef.current.srcObject = stream;
+  const toggleRecording = async () => {
+    if (!recording) {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      videoRef.current.srcObject = stream;
 
-    // Create a new recorder instance and set it in the state
-    const newRecorder = new RecordRTC(stream, { type: 'video' });
-    setRecorder(newRecorder);
+      const newRecorder = new RecordRTC(stream, { type: 'video' });
+      setRecorder(newRecorder);
 
-    // Start recording
-    newRecorder.startRecording();
-    setRecording(true);
+      newRecorder.startRecording();
+    } else {
+      recorder.stopRecording(() => {
+        const blob = recorder.getBlob();
+        setRecordedVideoBlob(blob);
+
+        previewVideoRef.current.src = URL.createObjectURL(blob);
+      });
+    }
+
+    // Toggle recording state
+    setRecording(!recording);
   };
 
-  const stopRecording = () => {
-    // Stop recording and update the state with the recorded blob
-    recorder.stopRecording(() => {
-      const blob = recorder.getBlob();
-      setRecordedVideoBlob(blob);
-
-      // Preview the recorded video
-      videoRef.current.src = URL.createObjectURL(blob);
-
-      // Reset the recorder state
-      setRecorder(null);
-
-      // Update the recording state
-      setRecording(false);
-    });
-  };
+  // Reset recorded video blob when the question changes
+  React.useEffect(() => {
+    setRecordedVideoBlob(null);
+  }, [question]);
 
   return (
     <div>
-      <div>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum magni reiciendis autem, iste dolor error laborum aut provident voluptate delectus?
-      </div>
+      <div>{question}</div>
 
-      {/* Video display */}
-      <video ref={videoRef} width="400" height="300" controls autoPlay muted></video>
-
-      {/* Record button */}
-      {!recording && (
-        <button onClick={startRecording}>Start Recording</button>
-      )}
-
-      {/* Stop recording button */}
-      {recording && (
-        <button onClick={stopRecording}>Stop Recording</button>
-      )}
-
-      {/* Display preview after stopping recording */}
-      {recordedVideoBlob && (
+      {/* Live recording on the left */}
+      <div className="flex mt-4">
         <div>
-          <h3>Preview</h3>
-          <video width="400" height="300" controls src={URL.createObjectURL(recordedVideoBlob)}></video>
+          <video ref={videoRef} width="400" height="300" controls autoPlay muted></video>
+          {/* Record button */}
+          <button className={`bg-[#ED9017] py-2 px-4 rounded-lg text-white mt-2`} onClick={toggleRecording}>
+            {recording ? 'Stop Recording' : 'Start Recording'}
+          </button>
         </div>
-      )}
+
+        {/* Preview on the right */}
+        {recordedVideoBlob && (
+          <div className="mt-[-40px] ml-auto flex justify-end ">
+            <video ref={previewVideoRef} width="400" height="300" controls src={URL.createObjectURL(recordedVideoBlob)}></video>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
