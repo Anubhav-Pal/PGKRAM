404b4a362cfae86a53f0608a95a2c45a1cf75a89
Anubhav Pal <anubhavpal15june@gmail.com>
2023-12-20 10:13:22 +0530

    enhanced

--- src/components/analytics/RetentionRate.jsx
+++ src/components/analytics/RetentionRate.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from '../Navbar';
 import Chart from 'react-apexcharts';
+import Select from 'react-select';
 
 const Dashboard = () => {
   const [chartData, setChartData] = useState({
